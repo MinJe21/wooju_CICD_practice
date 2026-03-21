8dc0cd2195fa98829a04949d45ef60db589e1033
01 <parkjua78@gmail.com>
2026-03-21 13:37:18 +0900

    Controller added

---
+++ src/main/java/Controller.java
@@ -0,0 +1,10 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class Controller {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, CD Test v1"; // 나중에 v2로 바꿔서 테스트할 부분입니다.
+    }
+}
